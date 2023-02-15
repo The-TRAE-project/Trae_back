ecb8330fae44a688b58b7c5f3e93d716cded690b
Vladimir Olennikov <ath31st@yandex.ru>
2023-02-15 14:11:00 +0300

    добавлен сваггер 3.0

--- src/main/java/ru/trae/backend/BackendApplication.java
+++ src/main/java/ru/trae/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package ru.trae.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebMvc
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/trae/backend/config/BeanConfig.java
@@ -0,0 +1,28 @@
+package ru.trae.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+}
