c43e32ac4cd5ef1e3e762391af28d2fcc07454ce
Vladimir Olennikov <ath31st@yandex.ru>
2023-09-04 10:21:13 +0300

    исправлен тест сокращенного дто маппера

--- src/test/java/ru/trae/backend/dto/mapper/ProjectShortDtoMapperTest.java
+++ src/test/java/ru/trae/backend/dto/mapper/ProjectShortDtoMapperTest.java
@@ -24,18 +24,19 @@ import ru.trae.backend.entity.task.Operation;
 import ru.trae.backend.entity.task.Project;
 
 class ProjectShortDtoMapperTest {
-  
+
   @Test
   void apply_WhenProjectHasOperations_ShouldMapToProjectShortDtoWithOperationInfo() {
     //given
     Project project = new Project();
     project.setEndDateInContract(LocalDateTime.now().plusDays(30));
+    project.setRealEndDate(LocalDateTime.now().plusDays(28));
     project.setId(1L);
     project.setEnded(true);
     project.setNumber(100);
     project.setName("test_project");
     project.setCustomer("test_customer");
-    
+
     Operation o1 = new Operation();
     o1.setPlannedEndDate(LocalDateTime.now().plusDays(5));
     o1.setId(1L);
@@ -43,7 +44,7 @@ class ProjectShortDtoMapperTest {
     o1.setName("operation_1");
     o1.setInWork(true);
     o1.setReadyToAcceptance(false);
-    
+
     Operation o2 = new Operation();
     o1.setPlannedEndDate(LocalDateTime.now().plusDays(10));
     o2.setId(2L);
@@ -51,20 +52,20 @@ class ProjectShortDtoMapperTest {
     o2.setName("operation_2");
     o2.setInWork(true);
     o2.setReadyToAcceptance(true);
-    
+
     project.setOperations(List.of(o1, o2));
-    
+
     OperationInfoForProjectTemplateDtoMapper operationInfoForProjectTemplateDtoMapper
         = mock(OperationInfoForProjectTemplateDtoMapper.class);
     OperationInfoForProjectTemplateDto operationInfoDto
         = new OperationInfoForProjectTemplateDto(o2.getName(), o2.isEnded(), o2.isInWork(), o2.isReadyToAcceptance());
     when(operationInfoForProjectTemplateDtoMapper.apply(o2)).thenReturn(operationInfoDto);
-    
+
     ProjectShortDtoMapper projectShortDtoMapper = new ProjectShortDtoMapper(operationInfoForProjectTemplateDtoMapper);
-    
+
     //when
     ProjectShortDto projectShortDto = projectShortDtoMapper.apply(project);
-    
+
     //then
     assertEquals(project.getId(), projectShortDto.id());
     assertEquals(project.isEnded(), projectShortDto.isEnded());
@@ -72,7 +73,7 @@ class ProjectShortDtoMapperTest {
     assertEquals(project.getName(), projectShortDto.name());
     assertEquals(project.getCustomer(), projectShortDto.customer());
     assertEquals(operationInfoDto, projectShortDto.operation());
-    
+
     verify(operationInfoForProjectTemplateDtoMapper).apply(o2);
   }
 }
